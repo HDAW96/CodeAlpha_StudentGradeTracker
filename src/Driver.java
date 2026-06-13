fb14d683573c0dfdd99781f86246422395862600
yaman <yaman-awad-yolo@hotmail.com>
2026-06-13 18:12:35 +0300

    First Commitment of StudentGradeTracker

---
+++ src/Driver.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Driver {
+
+  private static ArrayList grades;
+
+  public static void main(String[]args){
+  ArrayList<Integer> grades = new ArrayList<>();
+  ArrayList<String> students = new ArrayList<>();
+  }
+
+
+}
